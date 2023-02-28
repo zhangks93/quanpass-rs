aa540e8ba41068c9dc5640ee0f138556deb905da
zhangks <zhangks93@outlook.com>
2023-02-28 11:31:00 +0800

    bugfix

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use robot::robot::MANAGER;
 #[tokio::main]
 async fn main() {
     Robot::append(Robot::new(String::from("Jack"), String::from("Grid")));
-    trade::crypto_client::CryptoClient::new().limit_buy("DOGEBUSD", 125.5, 0.08158);
+    trade::crypto_client::CryptoClient::new().limit_buy("DOGEBUSD", 150, 0.08158);
 
     loop {
         unsafe {
